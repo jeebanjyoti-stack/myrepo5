4d5097db78917a95bf43c0e488b4070b173c44f6
jeeban <jeebanjyoti193@gmail.com>
2023-07-11 11:54:47 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @goto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String username;
+            Console.WriteLine("enter user name");
+            username = Console.ReadLine();
+            if (username.Length>=6) {
+
+                Console.WriteLine("Welcome :  \t " + username);
+
+            }
+            else
+            {
+                Console.WriteLine("invalid user name");
+            }
+            Console.ReadKey();
+        }
+    }
+}
